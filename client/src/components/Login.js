e2c1be198698685a87ac2f3a2b315a2ea95184a5
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2023-08-30 11:02:49 -0600

    add the dumb lil form

---
+++ client/src/components/Login.js
@@ -0,0 +1,49 @@
+import {useCallback} from 'react';
+
+
+const Login = () => {
+    const openGoogleLoginPage = useCallback(() => {
+        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+      
+        const scope = [
+          'https://www.googleapis.com/auth/userinfo.email',
+          'https://www.googleapis.com/auth/userinfo.profile'
+        ].join(' ');
+
+        let redirectUri = `${process.env.REACT_APP_BASE_BACKEND_URL}${process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URI}`;
+        debugger
+      
+        const params = {
+          response_type: 'code',
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          redirect_uri: redirectUri,
+          prompt: 'select_account',
+          access_type: 'offline',
+          scope
+        };
+      
+        const urlParams = new URLSearchParams(params).toString();
+      
+        window.location = `${googleAuthUrl}?${urlParams}`;
+      }, []);
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(e)
+    }
+
+    return <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input id="email"/>
+        <label htmlFor="email">Password</label>
+        <input id="password"/>
+        <input type="submit" value="Login"/>
+      </form>
+        or
+        <br/>
+        <button onClick={openGoogleLoginPage}>Login with google</button>
+    </div>
+}
+
+export default Login;
---
+++ client/src/my-api/login.js
@@ -0,0 +1,4 @@
+
+const login = (username, password) => {
+    return "hey"
+}
